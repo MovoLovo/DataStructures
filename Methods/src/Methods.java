24701d79a54c0d372e85d6ad7a7c735cbd1a4049
Spencilian <spencerli253@gmail.com>
2020-10-16 23:31:14 -0400

    Resetting

--- Methods/src/Methods.java
+++
@@ -1,8 +0,0 @@
-public class Methods {
-    static RoosterData r;
-    public static void main(String[] args) {
-        r = new RoosterData();
-        System.out.println("Total Rooster Volume: " + r.totalRoosterVolume(1.823));
-    }
-}
-
